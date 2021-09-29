b9535cd757bd69cd9762577d8fe003ad633aabf2
paulomartinsat <paulomartinsat@gmail.com>
2021-09-29 18:29:47 -0300

    set enum type to make easier development of methods

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
-  belongs_to :wallet, class_name: "Wallet", foreign_key: "origin_wallet_id", dependent: :destroy
-  belongs_to :wallet, class_name: "Wallet", foreign_key: "destination_wallet_id", dependent: :destroy
+  belongs_to :wallet
+  
   enum type: { credit: 0, debit: 1}
 end
