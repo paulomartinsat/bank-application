f7872d1ead2841309823aa6fcfe845894efc3456
paulomartinsat <paulomartinsat@gmail.com>
2021-09-29 18:28:13 -0300

    set default balance of 100

--- db/migrate/20210927134843_create_wallets.rb
+++ db/migrate/20210927134843_create_wallets.rb
@@ -1,7 +1,7 @@
 class CreateWallets < ActiveRecord::Migration[6.1]
   def change
     create_table :wallets do |t|
-      t.decimal :balance
+      t.decimal :balance, default: 100
       t.string :history
 
       t.timestamps
